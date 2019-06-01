e589381e48d0e2a02839896c880f8f30f9ed7806
Marko Jagor <majago@ttu.ee>
2019-06-01 17:35:01 +0300

    Order update

--- src/main/java/models/Order.java
+++ src/main/java/models/Order.java
@@ -2,8 +2,10 @@ package models;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Table(name = "Orders")
@@ -12,7 +14,21 @@ public class Order {
     private String id;
     private String clientName;
     private String offerId;
-    private BigDecimal offerPrice;
+    private BigDecimal orderPrice;
+
+    @OneToMany
+    private List<FoodItem> foodItems;
+
+    public Order() {
+
+    }
+
+    public Order(String clientName, List<FoodItem> foodItems, BigDecimal orderPrice) {
+        this.clientName = clientName;
+        this.foodItems = foodItems;
+//this.orderPrice = foodItems.stream().map(foodItems -> foodItems.getPrice()).sum();
+        //this.orderPrice = foodItems.stream().mapToLong(FoodItem::getPrice).sum();
+    }
 
 
     @Id
@@ -40,11 +56,19 @@ public class Order {
         this.offerId = offerId;
     }
 
-    public BigDecimal getOfferPrice() {
-        return offerPrice;
+    public BigDecimal getOrderPrice() {
+        return orderPrice;
     }
 
-    public void setOfferPrice(BigDecimal offerPrice) {
-        this.offerPrice = offerPrice;
+    public void setOrderPrice(BigDecimal offerPrice) {
+        this.orderPrice = offerPrice;
+    }
+
+    public List<FoodItem> getFoodItems() {
+        return foodItems;
+    }
+
+    public void setFoodItems(List<FoodItem> foodItems) {
+        this.foodItems = foodItems;
     }
 }
